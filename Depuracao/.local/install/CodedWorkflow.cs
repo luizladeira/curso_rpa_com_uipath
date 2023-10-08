b7b42057465cbca8caa6fb66787fa8c89e4f2b4b
Luiz Ladeira <luiz.ladeira@coopmil.coop.br>
2023-10-08 15:26:31 -0300

    depuração fazendo conta com a calculadora do windows

---
+++ Depuracao/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace Depuracao
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
